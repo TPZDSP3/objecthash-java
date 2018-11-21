8388bf9bbb7abda8d2190887e174158cc182eb9b
Gideon Goldberg <gidsgoldberg@gmail.com>
2018-11-21 17:36:29 +0000

    remove TODO

--- src/main/java/ObjectHash.java
+++ src/main/java/ObjectHash.java
@@ -1,18 +1,18 @@
-import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.text.Normalizer;
 
-public class ObjectHash {
+public final class ObjectHash {
+    
+    
     private static final String STRING_TAG = "u";
 
-    public String hexDigest (String value) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+    public String hexDigest (String value) throws NoSuchAlgorithmException {
         MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
-        sha256.update(STRING_TAG.getBytes("UTF-8"));
-
-        // TODO: this needs to UTF8 encode and do unicode normalisation
+        sha256.update(STRING_TAG.getBytes(StandardCharsets.UTF_8));
         String normalisedValue = Normalizer.normalize(value, Normalizer.Form.NFC);
-        sha256.update(normalisedValue.getBytes("UTF-8"));
+        sha256.update(normalisedValue.getBytes(StandardCharsets.UTF_8));
         StringBuilder sb = new StringBuilder();
         for (byte b : sha256.digest()) {
             sb.append(String.format("%02x", b));
--- src/test/java/ObjectHashTest.java
+++ src/test/java/ObjectHashTest.java
@@ -9,13 +9,13 @@ import java.security.NoSuchAlgorithmException;
 
 public class ObjectHashTest {
     @Test
-    public void testStringHexDigest() throws NoSuchAlgorithmException, UnsupportedEncodingException {
+    public void hashesStrings() throws NoSuchAlgorithmException {
         ObjectHash objectHash = new ObjectHash();
         assertEquals(objectHash.hexDigest("Hello, Ruby!"), "c92765c1350e6df6800dbadedb3420f398d5f3c7e38a3da48aadb3332280f85f");
     }
 
     @Test
-    public void normalisesStrings() throws UnsupportedEncodingException, NoSuchAlgorithmException {
+    public void normalisesStrings() throws NoSuchAlgorithmException {
         ObjectHash classUnderTest = new ObjectHash();
         assertEquals(classUnderTest.hexDigest("\u03d3"), "f72826713a01881404f34975447bd6edcb8de40b191dc57097ebf4f5417a554d" );
         assertEquals(classUnderTest.hexDigest("\u03d2\u0301"), "f72826713a01881404f34975447bd6edcb8de40b191dc57097ebf4f5417a554d" );
