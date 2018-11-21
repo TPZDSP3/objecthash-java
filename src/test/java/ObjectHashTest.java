2befcee29f37ae29c06cb6d3ff0130b258dbb68f
Gideon Goldberg <gidsgoldberg@gmail.com>
2018-11-21 17:11:15 +0000

    add normalisation and UTF-8 encoding.

--- src/test/java/ObjectHashTest.java
+++ src/test/java/ObjectHashTest.java
@@ -4,11 +4,22 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
 
 public class ObjectHashTest {
-    @Test public void testStringHexDigest() throws NoSuchAlgorithmException {
-        ObjectHash classUnderTest = new ObjectHash();
-        assertEquals(classUnderTest.hexDigest("Hello, Ruby!"), "c92765c1350e6df6800dbadedb3420f398d5f3c7e38a3da48aadb3332280f85f");
+    @Test
+    public void testStringHexDigest() throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        ObjectHash objectHash = new ObjectHash();
+        assertEquals(objectHash.hexDigest("Hello, Ruby!"), "c92765c1350e6df6800dbadedb3420f398d5f3c7e38a3da48aadb3332280f85f");
     }
+
+    @Test
+    public void normalisesStrings() throws UnsupportedEncodingException, NoSuchAlgorithmException {
+        ObjectHash classUnderTest = new ObjectHash();
+        assertEquals(classUnderTest.hexDigest("\u03d3"), "f72826713a01881404f34975447bd6edcb8de40b191dc57097ebf4f5417a554d" );
+        assertEquals(classUnderTest.hexDigest("\u03d2\u0301"), "f72826713a01881404f34975447bd6edcb8de40b191dc57097ebf4f5417a554d" );
+    }
+
+
 }
