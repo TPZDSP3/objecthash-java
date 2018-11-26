b5d73ba53501e0717db24d83348493b6b6fd08ab
Gideon Goldberg <gidsgoldberg@gmail.com>
2018-11-26 16:36:06 +0000

    handle string -> stringt case

--- src/main/java/uk/gov/objecthash/ObjectHash.java
+++ src/main/java/uk/gov/objecthash/ObjectHash.java
@@ -3,9 +3,15 @@ package uk.gov.objecthash;
 import com.fasterxml.jackson.databind.JsonNode;
 
 import java.time.Instant;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 public final class ObjectHash {
     public static String toHexDigest(String value)  {
@@ -35,10 +41,18 @@ public final class ObjectHash {
     public static String toHexDigest(byte[] value) {
         return new RawValue(value).hexDigest();
     }
-    
+
     public static String toHexDigest(JsonNode json) {
-        
-        return "foo";
-        
+        Iterator<Map.Entry<String, JsonNode>> fields = json.fields();
+        var stream = StreamSupport.stream(Spliterators.spliteratorUnknownSize(fields, Spliterator.ORDERED), false);
+        return stream.map(node -> {
+                    var key = node.getKey();
+                    var value = node.getValue().asText();
+                    Map<String, ObjectHashable> data = new HashMap<>(){{
+                        put(key, new StringValue(value));
+                    }};
+                    return ObjectHash.toHexDigest(data);
+                }
+        ).collect(Collectors.joining(""));
     }
 }
